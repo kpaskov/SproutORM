7889ab26e4543c90336363b662d117cbbc062413
kpaskov <kpaskov@stanford.edu>
2013-07-25 09:54:01 -0700

    Working with buildout.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'cx_Oracle',
+    #'cx_Oracle',
     'sqlalchemy'
 ]
 
