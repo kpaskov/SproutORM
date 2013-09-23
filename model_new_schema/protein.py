bd0febd6344b3bc6cbb88e96f911f4bf0435da85
kpaskov <kpaskov@stanford.edu>
2013-09-23 10:17:22 -0700

    Rename domain table to pdomain.

--- model_new_schema/protein.py
+++ model_new_schema/protein.py
@@ -33,30 +33,32 @@ class Protein(Bioentity):
         self.c_term_seq = c_term_seq
         
 class Domain(Base, EqualityByIDMixin):
-    __tablename__ = "domain"
+    __tablename__ = "pdomain"
     
-    id = Column('domain_id', Integer, primary_key=True)
+    id = Column('pdomain_id', Integer, primary_key=True)
     source = Column('source', String)
-    db_identifier = Column('db_identifier', String)
+    format_name = Column('format_name', String)
+    display_name = Column('display_name', String)
     description = Column('description', String)
     interpro_id = Column('interpro_id', String)
     interpro_description = Column('interpro_description', String)
     link = Column('obj_link', String)
     
-    def __init__(self, source, db_identifier, description, 
-                 interpro_id, interpro_description, link):
+    def __init__(self, format_name, display_name, description, 
+                 interpro_id, interpro_description, link, source):
         self.source = source
-        self.db_identifier = db_identifier
+        self.format_name = format_name
+        self.display_name = display_name
         self.description = description
         self.interpro_id = interpro_id
         self.interpro_description = interpro_description
         self.link = link
         
     def unique_key(self):
-        return (self.db_identifier, self.source)
+        return (self.format_name, self.source)
     
 class Domainevidence(Evidence):
-    __tablename__ = "domainevidence"
+    __tablename__ = "pdomainevidence"
     
     id = Column('evidence_id', Integer, ForeignKey(Evidence.id), primary_key=True)
     start = Column('start_index', Integer)
@@ -65,7 +67,7 @@ class Domainevidence(Evidence):
     status = Column('status', String)
     date_of_run = Column('date_of_run', String)
     protein_id = Column('protein_id', Integer, ForeignKey(Protein.id))
-    domain_id = Column('domain_id', Integer, ForeignKey(Domain.id))
+    domain_id = Column('pdomain_id', Integer, ForeignKey(Domain.id))
        
     __mapper_args__ = {'polymorphic_identity': 'DOMAIN',
                        'inherit_condition': id==Evidence.id}
