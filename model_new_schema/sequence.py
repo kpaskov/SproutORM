c0881d85cdce1dccf59f163ffdefc2d8a34a2ffd
kpaskov <kpaskov@stanford.edu>
2013-09-23 15:38:18 -0700

    Added binding_evidence.

---
+++ model_new_schema/sequence.py
@@ -0,0 +1,32 @@
+'''
+Created on Sep 23, 2013
+
+@author: kpaskov
+'''
+from model_new_schema.bioentity import Bioentity
+from model_new_schema.evidence import Evidence
+from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.types import Integer, String
+
+class Bindingevidence(Evidence):
+    __tablename__ = "bindingevidence"
+    
+    id = Column('evidence_id', Integer, ForeignKey(Evidence.id), primary_key=True)
+    bioentity_id = Column('bioentity_id', Integer, ForeignKey(Bioentity.id))
+    total_score = Column('total_score', String)
+    expert_confidence = Column('expert_confidence', String)
+    img_url = Column('img_url', String)
+       
+    __mapper_args__ = {'polymorphic_identity': 'Binding',
+                       'inherit_condition': id==Evidence.id}
+
+    def __init__(self, evidence_id, experiment_id, reference_id, strain_id, source, 
+                 bioentity_id, total_score, expert_confidence, img_url,
+                 date_created, created_by):
+        Evidence.__init__(self, evidence_id, 'Binding', 
+                          experiment_id, reference_id, strain_id, source, None,
+                          date_created, created_by)
+        self.bioentity_id = bioentity_id
+        self.total_score = total_score
+        self.expert_confidence = expert_confidence
+        self.img_url = img_url
